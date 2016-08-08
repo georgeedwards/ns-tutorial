d5d37719e5768b54be28183d57a14c0f2863ccbe
George Edwards <g.edwards@gas-sense.co.uk>
2016-08-08 11:19:26 +0100

    Step 4.9 Add an animation

--- app/views/list/list.js
+++ app/views/list/list.js
@@ -10,14 +10,19 @@ var pageData = new Observable({
     grocery: ""
 });
 
-exports.loaded = function (args) {
+exports.loaded = function(args) {
     page = args.object;
+    var listView = page.getViewById("groceryList");
     page.bindingContext = pageData;
 
     groceryList.empty();
     pageData.set("isLoading", true);
-    groceryList.load().then(function () {
+    groceryList.load().then(function() {
         pageData.set("isLoading", false);
+        listView.animate({
+            opacity: 1,
+            duration: 1000
+        });
     });
 };
 
