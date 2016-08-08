bbe2dc093d2c91605a531c5c6f1c4c6714caa031
George Edwards <g.edwards@gas-sense.co.uk>
2016-08-08 13:12:56 +0100

    Step 6.1 Customise the ios action bar

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -5,14 +5,18 @@ var user = new UserViewModel();
 var page;
 var email;
 
-exports.loaded = function(args) {
+exports.loaded = function (args) {
     page = args.object;
+    if (page.ios) {
+        var navigationBar = frameModule.topmost().ios.controller.navigationBar;
+        navigationBar.barStyle = UIBarStyle.UIBarStyleBlack;
+    }
     page.bindingContext = user;
 };
 
-exports.signIn = function() {
+exports.signIn = function () {
     user.login()
-        .catch(function(error) {
+        .catch(function (error) {
             console.log(error);
             dialogsModule.alert({
                 message: "Unfortunately we could not find your account.",
@@ -20,12 +24,12 @@ exports.signIn = function() {
             });
             return Promise.reject();
         })
-        .then(function() {
+        .then(function () {
             frameModule.topmost().navigate("views/list/list");
         });
 };
 
-exports.register = function() {
+exports.register = function () {
     var topmost = frameModule.topmost();
     topmost.navigate("views/register/register");
 };
