d29ae903c17e342423ac30fe4c5d10dc65876c18
George Edwards <g.edwards@gas-sense.co.uk>
2016-08-08 10:43:39 +0100

    Step 3.5 Binding with view models

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,9 +1,16 @@
 var frameModule = require("ui/frame");
+var Observable = require("data/observable").Observable;
 var page;
 var email;
 
+var user = new Observable({
+    email: "user@domain.com",
+    password: "password"
+});
+
 exports.loaded = function(args) {
     page = args.object;
+    page.bindingContext = user;
 };
 
 exports.signIn = function() {
