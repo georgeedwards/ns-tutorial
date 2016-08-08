7a9f2636d4056b3f7c5b7b7d6c01bf07930ef54d
George Edwards <g.edwards@gas-sense.co.uk>
2016-08-08 11:04:25 +0100

    Step 4.6 Populate List View from Backend

--- app/views/list/list.js
+++ app/views/list/list.js
@@ -1,17 +1,18 @@
 var dialogsModule = require("ui/dialogs");
 var Observable = require("data/observable").Observable;
 var ObservableArray = require("data/observable-array").ObservableArray;
+var GroceryListViewModel = require("../../shared/view-models/grocery-list-view-model");
 var page;
 
+var groceryList = new GroceryListViewModel([]);
 var pageData = new Observable({
-    groceryList: new ObservableArray([
-        { name: "eggs" },
-        { name: "bread" },
-        { name: "cereal" }
-    ])
+    groceryList: groceryList
 });
 
 exports.loaded = function(args) {
     page = args.object;
     page.bindingContext = pageData;
+
+    groceryList.empty();
+    groceryList.load();
 };
