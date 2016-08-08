6ef63c21cd7f42fe5f190fb7bd5a7411528b944e
George Edwards <g.edwards@gas-sense.co.uk>
2016-08-08 10:15:25 +0100

    Step 3.2 Connecting tap events

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,3 +1,7 @@
-exports.loaded = function() {
-    console.log("hello");
+exports.signIn = function() {
+    alert("Signing in");
+};
+
+exports.register = function() {
+    alert("Registering");
 };
